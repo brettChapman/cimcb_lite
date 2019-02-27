d68479b52936dc3bd5dd6f055fdd3c6aede0cf1f
KevinMMendez <35716119+KevinMMendez@users.noreply.github.com>
2019-02-27 20:19:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="This is a pre-release.",
     long_description=long_description,
     license="http://www.apache.org/licenses/LICENSE-2.0.html",
-    url="https://github.com/KevinMMendez/cimcbtest",
+    url="https://github.com/KevinMMendez/cimcb_lite",
     packages=["cimcb_lite", "cimcb_lite.bootstrap", "cimcb_lite.cross_val", "cimcb_lite.model", "cimcb_lite.plot", "cimcb_lite.utils"],
     install_requires=[
         "numpy",
